20c08b8991fae011b7eb9ef3c4a128bfbef280c4
a-wahab-jafar <a.wahab.jafar1@gmail.com>
2026-02-22 21:26:56 +0300

    coderabbit fixes

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,7 +1,9 @@
 import { BACKEND_BASE_URL } from "@/constants";
 import { ListResponse } from "@/types";
 import { CreateDataProviderOptions, createDataProvider } from "@refinedev/rest";
-
+if (!BACKEND_BASE_URL) {
+  throw new Error("BACKEND_BASE_URL is not defined in environment variables");
+}
 const options: CreateDataProviderOptions = {
   getList: {
     getEndpoint: ({ resource }) => resource,
@@ -52,12 +54,12 @@ const options: CreateDataProviderOptions = {
 
 
       mapResponse: async (response) => {
-      const payload: ListResponse = await response.json();
+      const payload: ListResponse = await response.clone().json();
       return payload.data ?? [];
     },
 
     getTotalCount: async (response) => {
-      const payload: ListResponse = await response.json();
+      const payload: ListResponse = await response.clone().json();
       return payload.pagination?.total ?? payload.data?.length ?? 0;
     },
   },
